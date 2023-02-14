013be0a3517c26a0dd6dbdb69b2bb4417b6603b9
unknown <sxcueto@gmail.com>
2023-02-13 20:40:20 -0500

    push

---
+++ app.js
@@ -0,0 +1,33 @@
+//1.
+var a = 45;
+var b = 3;
+var c = 1;
+var d = 10;
+var e = 4;
+var f = 15;
+var g = 8;
+var h = 22;
+var i = 34;
+var j = 27;
+
+//2
+console.log (g + h);
+console.log (i + a);
+
+//3
+console.log (j-b);
+console.log(i-f);
+
+//4
+console.log(e*c);
+console.log(d*b);
+
+// 5
+console.log(i/g);
+console.log(a/b);
+// 6
+console.log(c++);
+console.log(d++);
+// 7
+console.log(f--);
+console.log(a--);
